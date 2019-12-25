485adce60fc9a468ad7e4aed8c7a180a5bc8db21
VuNhatLe\LeIT <vunhatledhbkhn@gmail.com>
2019-12-25 23:34:36 +0700

    Task-management-system
    Tasks

---
+++ HCI/admin/includes/sidebar.php
@@ -0,0 +1,76 @@
+     <aside id="slide-out" class="side-nav white fixed">
+                <div class="side-nav-wrapper">
+                    <div class="sidebar-profile">
+                        <div class="sidebar-profile-image">
+                            <img src="../assets/images/profile-image.png" class="circle" alt="">
+                        </div>
+                        <div class="sidebar-profile-info">
+                       
+                                <p>Admin</p>
+
+                         
+                        </div>
+                    </div>
+            
+                <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion">
+                    <li class="no-padding"><a class="waves-effect waves-grey" href="dashboard.php"><i class="material-icons">settings_input_svideo</i>Dashboard</a></li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">apps</i>Groups<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="adddepartment.php">Add Group</a></li>
+                                <li><a href="managedepartments.php">Manage Groups</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">code</i>Projects<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addleavetype.php">Add Project</a></li>
+                                <li><a href="manageleavetype.php">Manage Projects</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">account_box</i>Employees<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addemployee.php">Add Employee</a></li>
+                                <li><a href="manageemployee.php">Manage Employee</a></li>
+       
+                            </ul>
+                        </div>
+                    </li>
+
+   <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">desktop_windows</i>Task Management<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="leaves.php">All Tasks</a></li>
+                                <li><a href="addtasks.php">Add Tasks</a></li>
+                                <li><a href="pending-leavehistory.php">Pending Tasks </a></li>
+                                <li><a href="approvedleave-history.php">Approved Tasks</a></li>
+                                  <li><a href="notapproved-leaves.php">Not Approved Tasks</a></li>
+       
+                            </ul>
+                        </div>
+                    </li>
+
+
+
+
+                        <li class="no-padding">
+                                <a class="waves-effect waves-grey" href="logout.php"><i class="material-icons">exit_to_app</i>Sign Out</a>
+                            </li>  
+                 
+
+                 
+              
+                </ul>
+                   <div class="footer">
+                    <p class="copyright">HCI-LTU14 <a href="http://www.code-projects.org/"> Vũ Nhật Lệ </a></p>
+                
+                </div>
+                </div>
+            </aside>
